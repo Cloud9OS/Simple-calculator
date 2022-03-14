fda491559cfda56cdaaf99bb048cce810b52c14f
Ahmed Alderbas <95334509+Cloud9OS@users.noreply.github.com>
2022-03-14 03:50:41 +0300

    Create cal.py

---
+++ cal.py
@@ -0,0 +1,19 @@
+print("your operators +,-,*,/")
+num = input("put your first number: ")
+operator = input("put your OPS: ")
+num2 = input("put your second number: ")
+
+def cal():
+    try:
+        if operator == "+":
+            return float(num)+float(num2)
+        if operator == "-":
+            return float(num)-float(num2)
+        if operator == "*":
+            return float(num)*float(num2)
+        if operator == "/":
+            return float(num)/float(num2)
+    except ValueError as err:
+        print(err)
+
+print(cal())
