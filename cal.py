0d058eff3372e7535ca0aff9514f4b6d6f9bf067
Ahmed Alderbas <95334509+Cloud9OS@users.noreply.github.com>
2022-03-14 06:16:47 +0300

    Update cal.py

--- cal.py
+++ cal.py
@@ -1,19 +1,28 @@
-print("your operators +,-,*,/")
-num = input("put your first number: ")
-operator = input("put your operator: ")
-num2 = input("put your second number: ")
-
 def cal():
     try:
-        if operator == "+":
-            return float(num)+float(num2)
-        if operator == "-":
-            return float(num)-float(num2)
-        if operator == "*":
-            return float(num)*float(num2)
-        if operator == "/":
-            return float(num)/float(num2)
-    except ValueError as err:
-        print(err)
+        while True:
+            print("your operators +,-,*,/")
+            num = input("put your first number: ")
+            operator = input("put your operator: ")
+            num2 = input("put your second number: ")
 
-print(cal())
+            def operation():
+                if operator == "+":
+                    return float(num)+float(num2)
+                elif operator == "-":
+                    return float(num)-float(num2)
+                elif operator == "*":
+                    return float(num)*float(num2)
+                else:
+                  operator == "/"
+                  return float(num)/float(num2)
+            print(operation())
+    except (ValueError, TypeError):
+        print("Invalid input")
+    except:
+        print("An Error")
+    cal()
+
+
+
+cal()
